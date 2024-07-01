637c1b5de6eae92fe97f4db130acb306dc264b4f
beknur altynbek <95119524+altynbek04@users.noreply.github.com>
2024-07-01 17:31:24 +0500

    keks

---
+++ hashtables.py
@@ -0,0 +1,8 @@
+hash_table = {
+    "apple": 5,
+    "banana": 3,
+    "orange": 2
+}
+
+for key, value in hash_table.items():
+    print("{}: {}".format(key, value))
---
+++ merge.py
@@ -0,0 +1,39 @@
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    L = arr[l:m+1]
+    R = arr[m+1:r+1]
+
+    i = j = 0
+    k = l
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, l, r):
+    if l < r:
+        m = (l + r) // 2
+        merge_sort(arr, l, m)
+        merge_sort(arr, m + 1, r)
+        merge(arr, l, m, r)
+
+arr = [12, 11, 13, 5, 6, 7]
+merge_sort(arr, 0, len(arr) - 1)
+print(arr)
